9f18b542f43d4b291756c5ba74386c16fd308c86
Lara Ulrich <52208794@edu.campus02.at>
2023-05-14 17:44:45 +0200

    added new Project from exercise3 - Calculator + Main

---
+++ src/main/java/at/campus02/bsd/Calculator.java
@@ -0,0 +1,25 @@
+package at.campus02.bsd;
+
+public class Calculator {
+
+
+    public double add(double number1, double number2){
+        double result = number1+number2;
+        return result;
+    }
+
+    public double minus(double number1, double number2){
+        double result = number1-number2;
+        return result;
+    }
+
+    public double divide(double number1, double number2){
+        double result = number1/number2;
+        return result;
+    }
+
+    public double multiply(double number1, double number2){
+        double result = number1*number2;
+        return result;
+    }
+}
---
+++ src/main/java/at/campus02/bsd/Main.java
@@ -0,0 +1,17 @@
+package at.campus02.bsd;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        double num1 = 10;
+        double num2 = 2;
+
+        Calculator calculator = new Calculator();
+        System.out.println("Addition: " + calculator.add(num1, num2));
+        System.out.println("Subtraction: " + calculator.minus(num1, num2));
+        System.out.println("Division: " + calculator.divide(num1, num2));
+        System.out.println("Multiplication: " + calculator.multiply(num1, num2));
+
+    }
+}
