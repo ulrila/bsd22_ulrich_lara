7fd064b160de79a477f4ed33fec85c363b5dbb2f
Lara Ulrich <52208794@edu.campus02.at>
2023-05-25 13:31:22 +0200

    added CalculatorTest.java Class with 3 Tests for every Method from the Calculator

---
+++ src/test/java/at/campus02/bsd/CalculatorTest.java
@@ -0,0 +1,95 @@
+package at.campus02.bsd;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator;
+
+    @BeforeEach
+    public void setUp(){
+        calculator = new Calculator();
+    }
+
+    @AfterEach
+    public void tearDown(){
+        calculator = null;
+    }
+
+    /*
+     * Tests for add Method
+     */
+    @Test
+    public void testMethodAdd1(){
+        assertEquals(3, calculator.add(1.5, 1.5));
+    }
+
+    @Test
+    public void testMethodAdd2(){
+        assertEquals(4, calculator.add(7, -3));
+    }
+
+    @Test
+    public void testMethodAdd3(){
+        assertEquals(-6, calculator.add(2, -8));
+    }
+
+
+    /*
+     * Tests for minus Method
+     */
+    @Test
+    public void testMethodMinus1(){
+        assertEquals(1, calculator.minus(2, 1));
+    }
+
+    @Test
+    public void testMethodMinus2(){
+        assertEquals(11.5, calculator.minus(8, -3.5));
+    }
+
+    @Test
+    public void testMethodMinus3(){
+        assertEquals(-22, calculator.add(-15, 7));
+    }
+
+    /*
+     * Tests for divide Method
+     */
+    @Test
+    public void testMethodDivide1(){
+        assertEquals(5, calculator.divide(10, 2));
+    }
+
+    @Test
+    public void testMethodDivide2(){
+        assertEquals(8, calculator.divide(20, 2.5));
+    }
+
+    @Test
+    public void testMethodDivide3(){
+        assertEquals(-2, calculator.divide(-14, -7));
+    }
+
+    /*
+     * Tests for multiply Method
+     */
+    @Test
+    public void testMethodMultiply1(){
+        assertEquals(24, calculator.multiply(8, 3));
+    }
+
+    @Test
+    public void testMethodMultiply2(){
+        assertEquals(0, calculator.multiply(32, 0));
+    }
+
+    @Test
+    public void testMethodMultiply3(){
+        assertEquals(-72, calculator.multiply(18, -4));
+    }
+}
