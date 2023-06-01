16f9ed43ee984aa1e981b245f5a9b0676d4369c4
Lara Ulrich <52208794@edu.campus02.at>
2023-06-01 11:22:47 +0200

    Added the Logging object with an error log and info log

--- src/main/java/at/campus02/bsd/Main.java
+++ src/main/java/at/campus02/bsd/Main.java
@@ -1,8 +1,12 @@
 package at.campus02.bsd;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+
     public static void main(String[] args) {
 
+        Logger logger = LogManager.getLogger();
 
         double num1 = 10;
         double num2 = 2;
@@ -13,5 +17,13 @@ public class Main {
         System.out.println("Division: " + calculator.divide(num1, num2));
         System.out.println("Multiplication: " + calculator.multiply(num1, num2));
 
+        logger.info("It is a info logger.");
+        logger.error("It is an error logger.");
+
+
+
+
+
     }
+
 }
